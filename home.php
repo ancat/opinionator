cb63c23aba0c407b6a56cdfaf9fb9ea9bd69d3e6
xtao01 <tonythetigerttt@gmail.com>
2013-11-19 19:50:45 -0500

    elo function

--- home.php
+++ home.php
@@ -12,32 +12,38 @@ $pg = pg_connect("host=23.92.21.247 dbname=opinionator user=design password=p0st
 $result = pg_prepare($pg, "query", 'INSERT INTO products (product_name, product_image) VALUES ($1, $2)');
 $result = pg_execute($pg, "query", array("Bread Is A Losers", "http://i.imgur.com/Ojzw8r9.jpg"));
 
-$Ra = 1000;
-$Rb = 1000;
+function ELO_algorithm($winner = 1000, $loser = 1000){
+  $Ra = $winner;
+  $Rb = $loser;
 
-$k1 = 32;
 
-if (($Ra > 2099) && ($Ra < 2401)){
-	$k1 = 24;
+  $k1 = 32;
+
+  if (($Ra > 2099) && ($Ra < 2401)){
+    $k1 = 24;
+  }
+
+  if ($Ra>2400){
+    $k1 = 16; 
+  }
+
+  $k2 = 24;
+  $k3 = 16;
+
+  $Ea = 1/ (1+pow(10,(($Rb - $Ra)/400)));
+  $Eb = 1/ (1+pow(10,(($Ra - $Rb)/400)));
+
+  $Sa = 1;
+  $Sb = 0;
+
+  $Ra_prime = $Ra + $k1*($Sa - $Ea);
+  $Rb_prime = $Rb + $k1*($Sb - $Eb);
+
+  $array = [$Ra_prime, $Rb_prime];
+  return $array; 
 }
 
-if ($Ra>2400){
-	$k1 = 16; 
-}
 
-$k2 = 24;
-$k3 = 16;
-
-$Ea = 1/ (1+pow(10,(($Rb - $Ra)/400)));
-$Eb = 1/ (1+pow(10,(($Ra - $Rb)/400)));
-
-$Sa = 1;
-$Sb = 1;
-
-$Ra_prime = $Ra + $k1*($Sa - $Ea);
-$Rb_prime = $Rb + $k1*($Sb - $Eb);
-
-echo "$Ra_prime";
 
 
 $query= "SELECT * FROM products";
@@ -50,7 +56,7 @@ if ($stmt->execute()){
     echo "<td><b>job_num</b></td>";
     echo "<td><b>job</b></td>";
     echo "<td><b>industry</b></td>";
-    echo "</tr>";
+    hecho "</tr>";
 
     $stmt->bind_result($col1,$col2,$col3);
     while ($stmt->fetch()) {
