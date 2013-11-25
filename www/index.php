711fde8b2942074a58dba6bf6df8eecfbf74fac8
ancat <omar@etsy.com>
2013-11-25 07:08:21 +0000

    dynamic drop down

--- www/index.php
+++ www/index.php
@@ -2,5 +2,15 @@
     require_once 'includes/include_all.php';
 
     $tpl = $mustache->loadTemplate('index');
-    echo $tpl->render();
-?>
+
+    $result = pg_query($postgres, 'SELECT * FROM products');
+    if (!$result) {
+        die('error');
+    }
+
+    $code = '';
+    while($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
+        $code .= '<option value="' . $row['product_id'] . '">' . trim($row['product_name']) . '</option>' . "\n";
+    }
+
+    echo $tpl->render(array('drop_down' => $code));
