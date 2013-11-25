5ccee74c3f6df31b15d10213d0a172bec3e5ad14
ancat <omar@etsy.com>
2013-11-25 17:20:31 +0000

    basic error template and code

--- www/includes/include_all.php
+++ www/includes/include_all.php
@@ -1,4 +1,10 @@
 <?PHP
     require_once 'templating.php';
     require_once 'database.php';
+
+    function show_error($message) {
+        global $mustache;
+        $tpl = $mustache->loadTemplate('error');
+        echo $tpl->render(array('error_message' => $message));
+    }
 ?>
