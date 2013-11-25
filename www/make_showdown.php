4fe71a9487d4ffd2d8a175ffcabf395c22127361
ancat <omar@etsy.com>
2013-11-25 17:29:28 +0000

    oops

--- www/make_showdown.php
+++ www/make_showdown.php
@@ -32,7 +32,7 @@
     }
 
     $random_url = substr(md5(rand()*rand()), 0, 16);
-    $sekure_hmac = hash_hmac('md5', $random_url, 'strauss is an asshat');
+    $sekure_hmac = hash_hmac('md5', $random_url, 'why cant this semester be over already');
     $query = pg_prepare($postgres, 'create_url', 'INSERT INTO urls (url_unique_string, url_active, showdown_id) VALUES ($1, $2, $3)');
     $result = pg_execute($postgres, 'create_url', array($random_url, 1, $showdown_id));
     if (!$result) {
@@ -41,6 +41,6 @@
     }
 
     $tpl = $mustache->loadTemplate('make_showdown');
-    echo $tpl->render(array('product_url'=>$random_url, 'sekure_hmac'=>$sekure_hmac));
+    echo $tpl->render(array('product_url'=>$random_url, 'sekure_hmac'=>$sekure_hmac, 'host'=>$_SERVER['HTTP_HOST']));
 
 
